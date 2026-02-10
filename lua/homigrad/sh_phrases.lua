849ac8594f2232fca0044682de32decbd8ae5a54
CROW <juninorjohns@gmail.com>
2026-02-10 16:41:20 +0300

    the FUCK YOU SAY TO ME YOU LITTLE SHIT

--- lua/homigrad/sh_phrases.lua
+++ lua/homigrad/sh_phrases.lua
@@ -131,6 +131,16 @@ contextPhrases = {
 			"vo/streetwar/sniper/male01/c17_09_help01.wav", --
 			"vo/streetwar/sniper/male01/c17_09_help02.wav", --
 		},
+         ["Funny"] = {
+            "joewiththeguy.wav",
+            "bbq.wav",
+            "fuckyou.wav",
+            "swordsword.wav",
+            "tomholland.wav",
+            "momishomeless.wav",
+            "MEinMYROOM.wav",
+            "ninja.wav"
+            },
 		["Someone died"] = {
 			"vo/episode_1/npc/male01/cit_buddykilled01.wav",
 			"vo/episode_1/npc/male01/cit_buddykilled07.wav",
